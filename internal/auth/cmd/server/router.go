03ae2588d0ab938856fce45e7c2a0247a9972de2
taba2424 <tadashi-112@hotmail.co.jp>
2023-09-25 20:54:50 +0900

    fix: デバッグ処理の削除

--- internal/auth/cmd/server/router.go
+++ internal/auth/cmd/server/router.go
@@ -77,7 +77,7 @@ func (w *wrapResponseWriter) errorResponse() (*response.ErrorResponse, error) {
 
 func accessLogger(logger *zap.Logger, reg *registry) gin.HandlerFunc {
 	skipPaths := map[string]bool{
-		// "/health": true, // TODO: デバッグのため、一時的にコメントアウト
+		"/health": true,
 	}
 
 	return func(ctx *gin.Context) {
